97ef9caae4586393564a5e3b1be204982af19ad9
Leon <Leon@3cb770c6-e10f-a848-aa87-87e8d569c341>
2009-11-25 16:55:33 +0000

    -Implementar impresión en PDF de "HeadDetail" -Incluir alguna fuente más en el PDF.
    git-svn-id: svn://peter/Neptuno/Branches/Utilidades/papyro@18019 3cb770c6-e10f-a848-aa87-87e8d569c341

--- pruebas.py
+++ pruebas.py
@@ -1,60 +1,48 @@
 # -*- coding: utf-8 -*-
 
-from reports import Report
-#from reportPdf import ReportPdf
-from reportPlainText import ReportPlainText
 from unidadescompartidas import conexion
-#from reportlab.pdfgen import canvas
-#from reportlab.lib.pagesizes import A4
-from libpy.implementation.enviaremail import enviar_email
-import os.path
+from reports import Report
+from reportPdf import ReportPdf
+#from reportPlainText import ReportPlainText
+#from libpy.implementation.enviaremail import enviar_email
+#import os.path
 
 #f = file('./report1.xml', 'r')
 #f = file('D:/workspace/tandemdev/tandem/informes/resumen_mensual.xml', 'r')
 conector = conexion()
 
 #report_path = os.path.join(conector.datosconexion.getVariable('ruta_informes'), 
+#                           'MailResumenMensual/ResumenMensualEnglish.xml')
+
+#report_path = os.path.join(conector.datosconexion.getVariable('ruta_informes'), 
 #                           'MailResumenMensual/MailResumenMensual.xml')
 
-report_path = os.path.join(conector.datosconexion.getVariable('ruta_informes'), 
-                           'MailResumenMensual/MailResumenMensual.xml')
-
-f = file(report_path, 'r')
+#f = file(report_path, 'r')
+f = file('./report1.xml', 'r')
 try:    
     informe = Report(xml=f.read())    
 #    print informe.xml
     
-#    pdf = ReportPdf(informe, conector)    
-#    pdf.writeReport(pdf_file='./hello.pdf', params=[('NUMERO_ALUMNOS', '100', 'int')])
+    pdf = ReportPdf(informe, conector)
+    pdf.writeReport(pdf_file='./report1.pdf', params=[('NUMERO_ALUMNOS', '100', 'int')])
 
-    txt = ReportPlainText(informe, conector)
-    resultado = txt.writeReport(report_path=report_path,
-                                params=[('P_MES', 'Noviembre', 'str'),
-                                        ('P_MONTH', 'November', 'str'),
-                                        ('P_FECHA_LIMITE', '2 de Dicimebre', 'str'),
-                                        ('P_DEADLINE', 'December 2', 'str'),
-                                        ('P_HOY', '23 de Noviembre', 'str'),
-                                        ('P_TODAY', 'November 23', 'str'),
-                                        ('P_PROFESOR', '1', 'int'),
-                                        ('P_FECHA_INICIO', '01/11/2009', 'date'),
-                                        ('P_FECHA_FIN', '23/11/2009', 'date')], debug=False)
-    print resultado
-
-    print enviar_email(('atenea@tandem-madrid.com', 'Atenea - Tandem Madrid'),
-                       [('leon.domingo@ender.es', 'León Domingo Ortín'), 
-                        ('dgsalas@ender.es', 'Domingo G. Salas')],
-                       'Resumen mensual 2', resultado,
-                       'smtp.dipro.es', 'atenea@tandem-madrid.com', 'A1s2d3f4',
-                       nombre_remitente='Atenea - Tandem Madrid <atenea@tandem-madrid.com>',
-                       nombres_destinatarios=['León Domingo Ortín <leon.domingo@ender.es>',
-                                              'Domingo G. Salas <dgsalas@ender.es>'])
-    
-    print enviar_email('atenea@tandem-madrid.com', ['leon.domingo@ender.es', 'dgsalas@ender.es'],
-                       'Resumen mensual 2', resultado,
-                       'smtp.dipro.es', 'atenea@tandem-madrid.com', 'A1s2d3f4',
-                       nombre_remitente='Atenea - Tandem Madrid <atenea@tandem-madrid.com>',
-                       nombres_destinatarios=['León Domingo Ortín <leon.domingo@ender.es>',
-                                              'Domingo G. Salas <dgsalas@ender.es>'])
+#    txt = ReportPlainText(informe, conector)
+#    resultado = txt.writeReport(report_path=report_path,
+#                                params=[('P_MES', 'Noviembre', 'str'),
+#                                        ('P_MONTH', 'November', 'str'),
+#                                        ('P_FECHA_LIMITE', '2 de Dicimebre', 'str'),
+#                                        ('P_DEADLINE', 'December 2', 'str'),
+#                                        ('P_HOY', '23 de Noviembre', 'str'),
+#                                        ('P_TODAY', 'November 23', 'str'),
+#                                        ('P_PROFESOR', '1', 'int'),
+#                                        ('P_FECHA_INICIO', '01/11/2009', 'date'),
+#                                        ('P_FECHA_FIN', '23/11/2009', 'date')], debug=False)
+#    
+#    print resultado
+#    print enviar_email(('atenea@tandem-madrid.com', 'Atenea - Tandem Madrid'),
+#                       [('leon.domingo@ender.es', 'León Domingo Ortín')],
+#                       'Resumen mensual', resultado,
+#                       'smtp.dipro.es', 'atenea@tandem-madrid.com', 'A1s2d3f4')
     
 finally:
     f.close()
--- reportPdf.py
+++ reportPdf.py
@@ -3,6 +3,8 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
 import reports
 #from random import seed, randint
 
@@ -15,6 +17,7 @@ class ReportPdf(object):
         self.hg = 0
         self.wd = 0
         self.cur_page = None
+        self.debug = False
         
     def newPage(self):
         
@@ -44,7 +47,7 @@ class ReportPdf(object):
         self.canvas.translate(m_left, self.hg + m_bottom + pf_height)
         
         # dibujar espacio con los márgenes
-        self.canvas.rect(0, -self.hg, self.wd, self.hg)
+        #self.canvas.rect(0, -self.hg, self.wd, self.hg)
         
         # fuente
         self.canvas.setFont(self.report.font.name, self.report.font.size)
@@ -53,14 +56,24 @@ class ReportPdf(object):
         if self.cur_page.page_header != None:            
             self.writePageHeader(self.cur_page.page_header)
         
-    def writeReport(self, pdf_file, c=None, params=None):
+    def writeReport(self, pdf_file, c=None, params=None, debug=False):
+        
+        self.debug = debug
         
         # canvas
         if c != None:
             self.canvas = c
         else:
             self.canvas = canvas.Canvas(pdf_file, pagesize=A4)
-            
+      
+        # cargar fuentes
+        vera_f = TTFont('VeraSans', 'C:/WINDOWS/Fonts/Vera.ttf')
+        tahoma_f = TTFont('Tahoma', 'C:/WINDOWS/Fonts/Tahoma.ttf')
+        lucida_f = TTFont('LucidaSans', 'C:/WINDOWS/Fonts/lsans.ttf')
+        pdfmetrics.registerFont(vera_f)
+        pdfmetrics.registerFont(tahoma_f)
+        pdfmetrics.registerFont(lucida_f)
+        
         param_names = [param[0] for param in self.report.params.params]
             
         # params
@@ -95,7 +108,7 @@ class ReportPdf(object):
     
     def writeReporPage(self, page):
         
-        print 'ReportPage:', str(page)
+        if self.debug: print 'ReportPage:', str(page)
         
 #        # page_header
 #        if page.page_header != None:
@@ -254,9 +267,25 @@ class ReportPdf(object):
                 
         sql %= detail_dict
         
+        data = self.conector.conexion.execute(sql)
+
         min_y = y
         new_page = False
-        for ddata in self.conector.conexion.execute(sql):
+        
+        # detail:header
+        if detail.header != None:
+            if data.rowcount > 0:
+                y, new_page = self.writeBody(detail.header.body, y, mdata)            
+            else:
+                if detail.header.print_if_detail_is_empty:
+                    y, new_page = self.writeBody(detail.header.body, y, mdata)
+                    
+            if not new_page:
+                if y < min_y: min_y = y
+            else:
+                min_y = y      
+        
+        for ddata in data:
             y, new_page = self.writeBody(detail.body, y, mdata, ddata)
             if not new_page:
                 if y < min_y: min_y = y
@@ -327,7 +356,7 @@ class ReportPdf(object):
             out = out.replace(k2, par[1])
         
         # imprimir contenido por "stdout"
-        print out, y - (text.top * mm + text.height * mm), text.top, text.height
+        if self.debug: print out, y - (text.top * mm + text.height * mm), text.top, text.height
 
         sz = self.report.font.size
         if text.font.size != None:
@@ -374,7 +403,8 @@ class ReportPdf(object):
     
     def writeLine(self, line, y):
         
-        print 'Line: x1=%d, y1=%d, x2=%d, y2=%d' % \
+        if self.debug: 
+            print 'Line: x1=%d, y1=%d, x2=%d, y2=%d' % \
                 (line.x1, line.y1, line.x2, line.y2)
         
         self.canvas.saveState()
