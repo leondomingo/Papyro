6a8e64ff57309ac81842d101b652054a7b747ded
Leon <Leon@3cb770c6-e10f-a848-aa87-87e8d569c341>
2009-12-18 09:22:08 +0000

    git-svn-id: svn://peter/Neptuno/Branches/Utilidades/papyro@18519 3cb770c6-e10f-a848-aa87-87e8d569c341

--- designer.py
+++ designer.py
@@ -7,6 +7,27 @@ import pygtk
 pygtk.require('2.0')
 import gtk #, gobject, pango
 
+TIPO_PAGE = 'page'
+
+BTN_PAGEHEADER = 'page_header'
+BTN_PAGEFOOTER = 'page_footer'
+BTN_MASTER = 'master'
+BTN_DETAIL = 'detail'
+BTN_GROUPHEADER = 'group_header'
+BTN_GROUPFOOTER = 'group_footer'
+
+BANDAS_DISPONIBLES = {
+                      TIPO_PAGE:
+                      {
+                       BTN_PAGEHEADER: True,
+                       BTN_PAGEFOOTER: True,
+                       BTN_MASTER: False,
+                       BTN_DETAIL: False,
+                       BTN_GROUPHEADER: False,
+                       BTN_GROUPFOOTER: False
+                      },
+                     }
+
 class Designer(object):
     
     def __init__(self):        
@@ -45,29 +66,32 @@ class Designer(object):
         vbx_bandas = gtk.VBox(homogeneous=False)
         vbx_bandas.set_size_request(100, -1)
         
-        # page header
-        btn_pageheader = gtk.Button('Page Header')
-        vbx_bandas.pack_start(btn_pageheader, False, False, 2)
+        btn_color = gtk.ColorButton()
+        vbx_bandas.pack_end(btn_color, False, False, 2)       
+        
+        # page header        
+        self.btn_pageheader = gtk.Button('Page Header')
+        vbx_bandas.pack_start(self.btn_pageheader, False, False, 2)
         
         # page footer
-        btn_pagefooter = gtk.Button('Page Footer')
-        vbx_bandas.pack_start(btn_pagefooter, False, False, 2)
+        self.btn_pagefooter = gtk.Button('Page Footer')
+        vbx_bandas.pack_start(self.btn_pagefooter, False, False, 2)
         
         # master
-        btn_master = gtk.Button('Master')
-        vbx_bandas.pack_start(btn_master, False, False, 2)
+        self.btn_master = gtk.Button('Master')
+        vbx_bandas.pack_start(self.btn_master, False, False, 2)
         
         # group header
-        btn_gheader = gtk.Button('Group Header')
-        vbx_bandas.pack_start(btn_gheader, False, False, 2)
+        self.btn_gheader = gtk.Button('Group Header')
+        vbx_bandas.pack_start(self.btn_gheader, False, False, 2)
         
         # group footer
-        btn_gfooter = gtk.Button('Group Footer')
-        vbx_bandas.pack_start(btn_gfooter, False, False, 2)
+        self.btn_gfooter = gtk.Button('Group Footer')
+        vbx_bandas.pack_start(self.btn_gfooter, False, False, 2)
         
         # detail
-        btn_detail = gtk.Button('Detail')
-        vbx_bandas.pack_start(btn_detail, False, False, 2)
+        self.btn_detail = gtk.Button('Detail')
+        vbx_bandas.pack_start(self.btn_detail, False, False, 2)
         
         # datos de la banda
         vbx_datosbanda = gtk.VBox(homogeneous=False)
@@ -97,6 +121,8 @@ class Designer(object):
         self.window.add(vbx_principal)
         
         self.window.show_all()
+        
+        self.update_bandas_disponibles('body')
         
     def main(self):
         gtk.main()
@@ -135,6 +161,24 @@ class Designer(object):
         
     def btn_exit_clicked(self, widget):
         self.window.destroy()
+        
+    def update_bandas_disponibles(self, tipo):
+        
+        if BANDAS_DISPONIBLES.has_key(tipo):
+            self.btn_pageheader.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_PAGEHEADER])
+            self.btn_pagefooter.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_PAGEFOOTER])
+            self.btn_master.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_MASTER])
+            self.btn_detail.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_DETAIL])
+            self.btn_gheader.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_GROUPHEADER])
+            self.btn_gfooter.set_sensitive(BANDAS_DISPONIBLES[tipo][BTN_GROUPFOOTER])
+            
+        else:                                          
+            self.btn_pageheader.set_sensitive(True)
+            self.btn_pagefooter.set_sensitive(True)
+            self.btn_master.set_sensitive(True)
+            self.btn_detail.set_sensitive(True)
+            self.btn_gheader.set_sensitive(True)
+            self.btn_gfooter.set_sensitive(True)
                     
 if __name__ == '__main__':
     designer = Designer()
