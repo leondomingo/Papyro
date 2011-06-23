fa3bc8cfd3263048e61b2945932aa3621592b4b8
León Domingo <leon.domingo@ender.es>
2011-06-23 12:56:38 +0200

    New source distribution

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='paPyro',
+      version='1.0.0',
+      author='León Domingo',
+      author_email='leon.domingo@ender.es',
+      description=('A PDF report generator written in Python'),
+      #license=???,
+      keywords='pdf reports ender',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Utilities',
+      ],
+      url='http://www.ender.es',      
+      packages=['papyro'],
+      #package_data={
+      #      'papyro': ['*.*'],
+      #},
+      install_requires=[
+        'Neptuno2',
+        'ReportLab>=2.5',
+        'SQLAlchemy==0.6.7',
+        'lxml==2.2.7',
+      ],
+     )
